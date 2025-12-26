81a228f39a8fd9ed462a5ef088faf9c425972811
eelco <git@eelcoj.com>
2025-12-26 12:30:04 +0700

    Destroy orphaned users

--- app/models/workspace/members.rb
+++ app/models/workspace/members.rb
@@ -5,8 +5,14 @@ class Workspace
     included do
       has_many :members, dependent: :destroy
       has_many :users, through: :members
+
+      after_destroy_commit :purge_orphaned_users!
     end
 
     def add_member(to:, role: nil) = Member::Setup.new(workspace: self, user: to, role: role).save
+
+    private
+
+    def purge_orphaned_users! = User.left_joins(:members).where(members: { id: nil }).destroy_all
   end
 end
